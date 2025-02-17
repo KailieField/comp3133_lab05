7580b0f2419e580c4b3c0852e0d004044f553826
Kailie Field <kailie.field@gmail.com>
2025-02-16 21:59:13 -0500

    updating resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,1 +1,43 @@
 const Movie = require('./models/Movie');
+
+
+const resolvers = {
+
+    Query: {
+
+        movies: async () => await Movie.find(),
+        movie: async (_, { id }) => await Movie.findById(id),
+
+        },
+
+        Mutation: {
+
+            addMovieRecord: async(_, args) => {
+
+                const newMovie = new Movie(args)
+                await newMovie.save()
+                return newMovie
+
+            },
+
+            updateMovieRecord: async (_, { id, ...updates }) => {
+
+                return await Movie.findByIdAndUpdate(id, updates, { new: true})
+
+            },
+
+            deleteMovieRecord: async (_, { id }) => {
+
+                return await Movie.findByIdAndDelete(id)
+
+            },
+        }
+    
+};
+
+module.exports = resolvers;
+
+
+// -- note: 
+// REFERENCE: PRO MERN: distinct to Apollo Servers
+// (_, { }) the underscore refers to the parent, indicating we are ignoring it
